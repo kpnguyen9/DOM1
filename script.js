ecef5df2e46cf38ba085f3c2ae788b52b1a265fb
kpnguyen9 <kpnguyen2@gmail.com>
2021-12-22 20:23:35 -0600

    added working button, for loop

--- script.js
+++ script.js
@@ -28,10 +28,12 @@
 // console.dir(document);
 
 let containerDiv = document.getElementById("container");
+let containerLink = document.getElementById("linkDiv");
 
 let element1 = document.createElement("p1");
-let element2 = document.createElement("p2");
+let a = document.createElement("a");
 let element3 = document.createElement("p3");
+let h1 = document.getElementById("header");
 
 let list1 = document.createElement("li");
 let list2 = document.createElement("li");
@@ -40,14 +42,35 @@ let list3 = document.createElement("li");
 element1.textContent = "Hello hello hello";
 containerDiv.appendChild(element1);
 
+let linkText = document.createTextNode("Click here for more information");
+a.appendChild(linkText);
+a.className = "title text";
+a.href = "google.com";
+containerLink.appendChild(a);
+
 list1.textContent = "list 1";
+list2.textContent = "second list";
+list3.textContent = "3rd list object";
 containerDiv.appendChild(list1);
+containerDiv.appendChild(list2);
+containerDiv.appendChild(list3);
+
+let listArr = document.getElementsByTagName("li");
+// console.log(listArr);
 
 let familyButton = document.getElementById("button");
 // familyButton.classList.add("famButton");
 
 familyButton.addEventListener("click", () => {
-  element2.textContent = "Hello 2";
-  containerDiv.appendChild(element2);
-  // alert("hi");
+  element3.textContent = "Hello 3";
+  containerDiv.appendChild(element3);
+
+  for (let i = 0; i < listArr.length; i++) {
+    listArr[i].textContent = "for loop text content";
+    listArr[i].style.color = "cyan";
+  }
+
+  h1.textContent = "Hello Family";
+
+  familyButton.style.backgroundColor = "blue";
 });
